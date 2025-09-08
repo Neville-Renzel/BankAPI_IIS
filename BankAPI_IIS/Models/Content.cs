49627da89abdf113e2db6dfe4bfb9f8f6057a589
Neville Renzel <renzeloakpensions@gmail.com>
2025-09-08 18:12:28 +0100

    Initial commit - Database First API

---
+++ BankAPI_IIS/Models/Content.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankAPI_IIS.Models;
+
+public partial class Content
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Body { get; set; } = null!;
+
+    public string? Category { get; set; }
+
+    public string? Summary { get; set; }
+
+    public bool IsPublished { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime? PublishedDate { get; set; }
+
+    public string? Author { get; set; }
+}
